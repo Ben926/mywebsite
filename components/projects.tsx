37608ffdabbf214074e193cccbbd6fa161d20ec5
Ben926 <benjamin.koh.ts@gmail.com>
2025-03-19 13:35:27 +0800

    Edit threshold for section in view again

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 
 export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
+  const { ref } = useSectionInView("Projects", 0.4);
   
   return (
     <motion.section 
